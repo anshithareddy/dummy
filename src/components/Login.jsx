a5a44b3c5af5828394c853582c404dec111e4359
Anshitha Reddy <129685607+anshithareddy@users.noreply.github.com>
2025-01-21 15:03:07 +0000

    test5-login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [test4,setTest4]=useState('');
+  const [test5,setTest5]=useState('');
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
 
